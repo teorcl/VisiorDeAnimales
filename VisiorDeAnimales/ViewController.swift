839dd5fc63bfba2682c88ec2f4d0a7164db08a58
Teodoro <teo.calle.lara@gmail.com>
2022-11-25 01:47:53 -0500

    se agregan las funciones isAnimal, uploadAnimalImage y searchAnimal

--- VisiorDeAnimales/ViewController.swift
+++ VisiorDeAnimales/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private struct Const{
+        static let animals: [String] = ["paloma","perro","gato","caballo"]
+    }
+    
+    // MARK: -Outlets
+    @IBOutlet weak var animalTextField: UITextField!
+    @IBOutlet weak var animalImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        searchAnimal()
+    }
+    
+    private func searchAnimal(){
+        let wantedAnimal = animalTextField.text ?? ""
+        uploadAnimalImage(isAnimal: isAnimal(wantedAnimal: wantedAnimal), nameAnimal: wantedAnimal)
+    }
+    
+    private func uploadAnimalImage(isAnimal: Bool, nameAnimal: String){
+        animalImageView.image = isAnimal ? UIImage(named: nameAnimal) : UIImage(named: "default")
+    }
+    
+    private func isAnimal(wantedAnimal: String) -> Bool {
+        return Const.animals.contains(wantedAnimal)
+    }
 }
 
